67913c3318c590e1d396f40e27b3ab1a4d4d6470
amirkhan1092 <amirkhan1092@gmail.com>
2020-05-24 13:45:40 +0530

    kivy with map widget

--- useCase1.py
+++ useCase1.py
@@ -1,10 +1,13 @@
-# from kivy.garden.mapview import MapView
+from kivy.garden.mapview import MapView
 from kivy.app import App
 from kivy.uix.label import Label
 
 class MapViewApp(App):
     def build(self):
-        # mapview = MapView(zoom=11, lat=50.6394, lon=3.057)
-        return Label(text='hello Python')
+        mapview = MapView(zoom=11, lat=50.6394, lon=3.057)
+        return mapview
+        # return Label(text='hello Python')
 
 MapViewApp().run()
+
+
